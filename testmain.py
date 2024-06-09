3cd63036bbd51eaea299ea8cb8a1c68daa4d8ad8
RyzenMaster <nenaddamjanovic@hotmail.rs>
2024-06-09 23:42:35 +0200

    finishing exercise, multi cell text

--- testmain.py
+++ testmain.py
@@ -8,15 +8,17 @@ filepaths = glob.glob("textfiles/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
-    # with open(filepath, "r") as file:
-        # content = file.read()
+    with open(filepath, "r") as file:
+        content = file.read()
 
     # extracts filename from filepath
     filename = Path(filepath).stem
     filename = filename.title()
     pdf.add_page()
     pdf.set_font(family="times",size=18, style="B")
-    pdf.cell(w=50, h=20, txt=filename)
+    pdf.cell(w=50, h=20, txt=filename, ln=1)
+    pdf.set_font(family="times",size=12, style="I")
+    pdf.multi_cell(w=0, h=8, txt=content, border=1)
 
 pdf.output("output.pdf")
 
